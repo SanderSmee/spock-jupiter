e3b1b70e813f7446f2280bbf6dc4eda26c524388
Sander Smeman <sander.smeman@jdriven.com>
2018-08-29 08:57:12 +0200

    Basic project showing JUnit5 and Spock 1.2 on jupiter (vintage) engine

---
+++ src/test/groovy/spock/TryoutSpec.groovy
@@ -0,0 +1,13 @@
+package spock;
+
+import spock.lang.Specification;
+
+/**
+ *
+ */
+class TryoutSpec extends Specification {
+    def 'demo spec'() {
+        expect:
+            true
+    }
+}
---
+++ src/test/java/junit/JavaTest.java
@@ -0,0 +1,12 @@
+package junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JavaTest {
+
+    @Test
+    void basicTest() {
+        Assertions.assertTrue(true);
+    }
+}
